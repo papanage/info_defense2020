03e27dc74da9e86955fe50c3dc3adb53f773df13
Vasily Lusnikov <v.lusnikov@intabia.ru>
2020-10-23 10:03:52 +0700

    lab3 отладка алгоритма

--- src/ru/nsu/block/ByteUtils.java
+++ src/ru/nsu/block/ByteUtils.java
@@ -20,4 +20,35 @@ public class ByteUtils {
        // System.out.println("res = " + result);
         return result;
     }
+
+    public static long cycleShift(long base, long shift) {
+
+        shift = (shift % Rc5.BLOCK_HALF + Rc5.BLOCK_HALF) % Rc5.BLOCK_HALF;
+       // System.out.println("start = " + base + " shift = " + shift +  " " + Long.toBinaryString(base).length());
+        long temp = base;
+       // System.out.println(Long.toBinaryString(base));
+        base = base << shift;
+        //System.out.println(Long.toBinaryString(base));
+        base = base & 0xFFFFFFFFL;
+       // System.out.println(Long.toBinaryString(base));
+        temp = temp >> (32 -  shift);
+       // System.out.println(Long.toBinaryString(temp));
+        base = base | temp;
+        //System.out.println(Long.toBinaryString(base));
+        //System.out.println("res = " + base);
+        return base;
+    }
+
+
+
+
+
+
+    public static void main(String[] args) {
+        //cycleShift(cycleShift(4698668300966100165L, 52L), -52L);
+       long h =  cycleShift(cycleShift(2545587777L, 2934294345L),-2934294345L) ;
+        ///long h2 =  cycleShift(9710L, -2934294345L);
+       /// System.out.println(h % 4294967296L);
+        //System.out.println(h2 % 4294967296L);
+    }
 }
--- src/ru/nsu/block/Rc5.java
+++ src/ru/nsu/block/Rc5.java
@@ -2,11 +2,17 @@ package ru.nsu.block;
 
 import java.util.Arrays;
 import java.util.Vector;
-import java.util.stream.Stream;
 
 import static java.lang.Long.max;
 
 
+/**
+ * Rc5 block-chiefr
+ * java implementation
+ * @author Lusnikov Vasily
+ * FIT NSU 2020
+ * I use description of https://ru.wikipedia.org/wiki/RC5
+ */
 public class Rc5 {
 
     /**
@@ -17,8 +23,13 @@ public class Rc5 {
     /**
      * half of block size
      * usually equals machine word length
+     * for simply I use 32
      */
-    final int BLOCK_HALF = 32;
+    static final int BLOCK_HALF = 32;
+
+    /**
+     * modulo of Gayla field
+     */
     final long MOD = pow(2, BLOCK_HALF);
 
     /**
@@ -26,15 +37,35 @@ public class Rc5 {
      */
     final int ROUND_COUNT = 12;
 
+    /**
+     * magic constant - depends of BLOCK_HALF
+     * P = Odd((f-1)*2^BLOCK_HALF))
+     * Q = Odd((e-2)*2^BLOCK_HALF))
+     * f - gold section
+     * e - Euler number
+     */
     final Long P = 0xB7E15163L;
     final Long Q = 0x9E3779B9L;
 
+    /**
+     *  key
+     */
     byte[] key = new byte[KEY_LENGTH];
+
+    /**
+     * mass with part of key
+     * length of part is no more than long size
+     * use for work in Gayla field
+     */
     Vector<byte[]>  l_mass;
 
+    /**
+     * special array for constants
+     */
     Long[] s = new Long[2*(ROUND_COUNT+1)];
 
-    long A = 4, B = 5;
+    long A = 324324, B = -1432423;
+
     private void keyToWords() {
         l_mass = new Vector<>();
         for (int i = 0; i < KEY_LENGTH/(BLOCK_HALF/8); i++) {
@@ -47,7 +78,6 @@ public class Rc5 {
         s[0] = P % MOD ;
         for (int i = 1; i < 2*(ROUND_COUNT+1); i++) {
             s[i] = (s[i-1] + Q)  % MOD;
-            //System.out.println(s[i]);
         }
     }
 
@@ -55,35 +85,30 @@ public class Rc5 {
         long G = 0, H = 0;
         int i = 0, j = 0;
         for (int n = 0; n < max(l_mass.size()*2, 6*(ROUND_COUNT+1)); n++) {
-            G = (s[i] + G + H) << 3;
-            H = (ByteUtils.bytesToLong(l_mass.get(i)) + G + H ) << (G + H);
-            System.out.println(G + " " + H);
+            G = ByteUtils.cycleShift(s[i] + (G + H) % MOD, 3);
+            H = ByteUtils.cycleShift(ByteUtils.bytesToLong(l_mass.get(i)) + (G + H) % MOD ,  G + H);
             i = (i + 1) % 2*(ROUND_COUNT + 1);
             j = (j + 1) % l_mass.size();
         }
     }
 
     private void code() {
-        //long A = 0, B = 0;
-        System.out.println(A + " " + B);
+        System.out.println("start: " +A + " " + B);
         A = (A + s[0]) % MOD;
         B = (B + s[1]) % MOD;
-        for (int i = 1; i < ROUND_COUNT; i++) {
-            //System.out.println(A + " " + B);
-            System.out.println(" a = " + A + "  b = " + B);
-            A = (((A^B) << B) + s[2*i]) % MOD;
-            B = (((B^A) << A) + s[2*i + 1]) % MOD;
-
+        for (int i = 0; i < ROUND_COUNT; i++) {
+            A = (ByteUtils.cycleShift(A^B, B) % MOD + s[2*i]) % MOD;
+            B = (ByteUtils.cycleShift(B^A, A) % MOD + s[2*i + 1]) % MOD;
         }
     }
     private void decode() {
-        for (int i = ROUND_COUNT; i  > 0; i--) {
-            B = ((B - s[2*i + 1]) >> A) ^ A;
-            A = ((A - s[2*i]) >> B) ^ B;
+        for (int i = ROUND_COUNT - 1; i  >= 0; i--) {
+            B = (ByteUtils.cycleShift((B - s[2*i + 1] + MOD) % MOD, -A) % MOD) ^ A;
+            A = (ByteUtils.cycleShift((A - s[2*i] + MOD) % MOD, -B) % MOD) ^ B;
         }
         A = (A - s[0]) % MOD;
         B = (B - s[1]) % MOD;
-        System.out.println(A + " " + B);
+        System.out.println("res: " + A + " " + B);
     }
     private long pow(long x, long y) {
         long res = 1;
@@ -93,6 +118,10 @@ public class Rc5 {
         return res;
     }
 
+    /**
+     * start rc5
+     * better run on 64x
+     */
     public void start() {
         keyToWords();
         extendKey();
@@ -109,7 +138,7 @@ public class Rc5 {
         Rc5 rc5 = new Rc5();
         Arrays.fill(rc5.key, (byte) 1);
         rc5.key[34] = 3;
-        System.out.println(rc5.MOD);
+        //System.out.println(rc5.MOD);
         rc5.start();
         for (byte[] b : rc5.l_mass) {
             for (byte f : b) {
